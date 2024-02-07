3f60183db5ceca4c0dc8d5fdc5c3f355b9d3ae33
sugiralieva <zhansaya.sugiralieva@gmail.com>
2024-02-07 19:52:00 +0600

    Отобразила страницу register.html Добавила проверку имени пользователя с помощью регулярных выражений, чтобы пользователь мог вводить только английскией буквы. Не стала использовать валидаторы, т.к нужно было проверить только имя, а не всю форму. Также добавила проверку имени и email на уникальность, т.к эти поля обозначены как unique.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from flask import render_template, request, url_for, flash, redirect
 
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
@@ -35,16 +36,27 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    pattern =r"(^[a-zA-Z]+$)"
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        hashed_password = generate_password_hash(password)
-        new_user = User(username=username, email=email, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Ваша учетная запись успешно создана', 'successfully')
-        return redirect(url_for('login'))
+        if re.search(pattern, request.form['username']):
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+            existing_user = User.query.filter_by(username=username).first()
+            existing_email = User.query.filter_by(email=email).first()
+            if existing_user:
+                flash('Пользователь с таким именем уже существует', category='error')
+                return redirect(url_for('register'))
+            if existing_email:
+                flash('Пользователь с таким email уже существует', category='error')
+                return redirect(url_for('register'))
+            hashed_password = generate_password_hash(password)
+            new_user = User(username=username, email=email, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        else:
+            flash('Имя должно состоять только из латинских букв', 'error')
     return render_template('register.html')
 
 
